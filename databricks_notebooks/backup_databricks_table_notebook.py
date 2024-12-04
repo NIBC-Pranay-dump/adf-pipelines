504674a15a1a11258f51adaf72c80bdab7d42bf1
Marta Radziszewska <Marta.Radziszewska@nibc.com>
2024-12-04 11:34:59 +0000

    Merged PR 2207: Attempt to implement databricks notebooks via asset bundles
    This is the deploying the `databricks-notebooks` directory to databricks. Note that the path to the dev is different than the path to the other environments. This is because development path needs to reference `current_user` otherwise it errors out.
    
    This is the dev deployment bundle location:
    ![Screenshot 2024-12-03 at 14.44.55.png](https://dev.azure.com/NIBC/59b9b987-65c8-4616-8698-a2240ca2c182/_apis/git/repositories/ac1561d1-06fd-42d0-a2cf-f5cb21c85910/pullRequests/2207/attachments/Screenshot%202024-12-03%20at%2014.44.55.png)
    
    For all other deployments, this will be `/Workspace/adf-pipelines/{env}/files/...`.
    
    I have tested it on the dev moneyview pipeline:
    ![Screenshot 2024-12-04 at 10.22.27.png](https://dev.azure.com/NIBC/59b9b987-65c8-4616-8698-a2240ca2c182/_apis/git/repositories/ac1561d1-06fd-42d0-a2cf-f5cb21c85910/pullRequests/2207/attachments/Screenshot%202024-12-04%20at%2010.22.27.png)
    
    During review, could you pay attention to the devops pipelines order, please?
    Also the naming of the jobs, I'm not super sure about it.
    
    PS:
    This PR also adds a PR template.

---
+++ databricks_notebooks/backup_databricks_table_notebook.py
@@ -0,0 +1,116 @@
+# Databricks notebook source
+"""This script is used to backup all tables in a Databricks catalog to an external location.
+
+Grants required:
+- Write files / read files on external location
+"""
+
+from datetime import datetime, timezone
+
+
+def _get_tables(catalog_name: str) -> list:
+    """Get all tables in a catalog.
+
+    We want to skip all the views from each catalog and only back up the managed
+    and external tables.
+
+    Args:
+        catalog_name (str): The catalog name.
+
+    Returns:
+        list: A list of table names in the catalog.
+
+    """
+    tables = spark.sql(
+        f"SELECT table_schema, table_name FROM {catalog_name}.information_schema.tables WHERE table_type != 'VIEW'"
+    ).collect()
+
+    print(tables)
+
+    full_table_paths = [
+        f"{catalog_name}.{tuple(row)[0]}.{tuple(row)[1]}" for row in tables
+    ]
+
+    print(f"Tables in catalog `{catalog_name}`: {full_table_paths}")
+
+    return full_table_paths
+
+
+def _generate_table_paths(table_names: list[str]) -> dict[str, dict[str, str]]:
+    """Generate the input and output table paths.
+
+    Args:
+        table_names (list[str]): A list of table names to backup
+
+    Returns:
+        dict[str, dict[str, str]]: A dictionary of table names and their input and output paths.
+            example:
+            {
+                "table_name": {
+                    "input_table": "input_catalog.input_schema.input_table",
+                    "backup_table": "full_output_path_including_external_location"
+                }
+            }
+
+    """
+    # use timestamp as part of the backup folder
+    backup_timestamp = datetime.now(timezone.utc).isoformat(timespec="seconds")
+    print(f"Backup timestamp: {backup_timestamp}")
+
+    # crete the input table paths directly from storage account
+    table_paths = {
+        table_name: {
+            "input_table": table_name,
+            "backup_table": f"{backup_to_location_path}/databricks/{backup_timestamp}/{table_name}",
+        }
+        for table_name in table_names
+    }
+
+    print(f"The following tables will be backed up: {table_paths}")
+
+    return table_paths
+
+
+def _backup_data(input_table_name: str, output_table_path: str) -> None:
+    """Backup a table in Databricks to an external location."""
+    # Use writing to parquet instead of clone, as cloning is not supported
+    # on tables with column masking
+    df_to_back_up = spark.table(input_table_name)
+    df_to_back_up.write.format("parquet").mode("overwrite").save(output_table_path)
+
+    print(
+        f"Table `{input_table_name}` has been successfully backed up to "
+        f"`{output_table_path}`."
+    )
+
+
+def main(catalog_name: str, backup_to_location_path: str) -> None:
+    """Backup a table in Databricks to an external location.
+
+    Args:
+        catalog_name (str): The catalog name.
+        backup_to_location_path (str): The external location path where the table will be backed up.
+
+    """
+
+    print(
+        f"Backup specification: catalog_name: `{catalog_name}`, "
+        f"backup to location path: `{backup_to_location_path}`."
+    )
+    input_table_name = _get_tables(catalog_name)
+
+    tables_paths = _generate_table_paths(input_table_name)
+
+    for idx, (_, paths) in enumerate(tables_paths.items()):
+        print(f"Backing up datable {idx + 1} of {len(tables_paths)}")
+
+        _backup_data(paths["input_table"], paths["backup_table"])
+
+    print(f"Backup of catalog `{catalog_name}` completed.")
+
+
+if __name__ == "__main__":
+    catalog_name = dbutils.widgets.get("catalog_name")
+    backup_to_location_path = dbutils.widgets.get("backup_to_location_path")
+
+    main(catalog_name=catalog_name, backup_to_location_path=backup_to_location_path)
---
+++ databricks_notebooks/decode_moneyView_base64_cp1252.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+#this notebook is used to have the correct encoding of moneyView files. this notebook is used in pl_custom_moneyview.
+import base64 
+import json
+
+#fetch the value from adf trigger
+inputv=dbutils.widgets.get("adf_input_value")
+
+#correctly decode the variable
+csv_string = base64.b64decode(inputv).decode("cp1252")
+
+#return a variable on notebook exit
+dbutils.notebook.exit(json.dumps({"adf_output_value":csv_string}))
